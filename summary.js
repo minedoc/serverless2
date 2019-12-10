bd7fd652be336c7fa82b17b1c6561c063a18aaea
david <david>
2019-12-10 22:35:00 +1100

    explain how data is structured

--- summary.js
+++ summary.js
@@ -17,21 +17,39 @@ function Database(crdt, idb, contentKey, indices) {
 }
 
 /*
-data structure
-data = Map<Key, Val>
-  update by linking to the operation (not the Key!)
-  Key: Array<Int|Str>
-  Value: JSON
+replication data model
+  tree of edits
+    child indicates causal relationship and positioning
+      {type: add, parent: parent, {key, value}}
+      {type: edit, parent: last-op, value}
+      {type: delete, parent: last-op}
+      {type: insertAfter, parent: prev, value}
+    siblings have an order
+    sum(sort(edits)) is actual data
+    each edit applies to a local part of data
+      rewinding entire state is not necessary
+    consider edits x -> y -> z now see x -> a -> b
+      a < y : a and b are deleted if non-commuting
+      a > y : y and z are deleted if non-commuting
+      1 = [add, null, {a: 5}], 2 = [add, 1, {b, 6}], 3 = [add, 1, {c, {}}],
 
-virtual fields - not stored in any thing
-order maintenance problem
-  insert(after=x, value=y)
-  isBefore(x, y)
+actual data
+  data = Map<Key, Val>
+    update by linking to the most recent operation (dont need key)
+    Key: Array<Int|Str>
+    Value: Object
+  virtual fields - local use only (for comparison)
+    not necessary maybe
+  order maintenance problem
+    insertAfter(x, entry)
+    find(x) - returns an iterator
+    isBefore(x, y)
 
 
 operation = {
   method: String,
-  data: Repeated(JSON),
+  id: String, table?
+  data: JSON,
   input: Ref,
   output: Ref,
 }
