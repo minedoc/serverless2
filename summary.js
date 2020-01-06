25fd780df471b3652ef8612eeee5a22c0af6747e
david <david>
2020-01-06 20:20:36 +1100

    change

--- summary.js
+++ summary.js
@@ -1,27 +1,23 @@
-// peerKeys and contentKeys are optional symmetric reader keys
-// integrityKeys are asymmetric writer keys
-function Discovery(server, name, peerKey) {
+function Discovery(server, name) {
   return {onPeer};
 }
 
-function Gossip(discovery, integrityKeys) {
+function Gossip(discovery) {
   const peers = new Map();
   const seen = new Map();
   const entryWatcher = new Observer();
-  const server = {
+  const serverDefinition = {
     push: [PushRequest, PushResponse, req => {
-      if (integrityCheck(req, integrityKeys)) {
-        if (!seen.has(req.entry)) {
-          entryWatcher.notify(req.entry);
-          seen.set(req.entry, 1);
-        }
+      if (!seen.has(req.entry)) {
+        entryWatcher.notify(req.entry);
+        seen.set(req.entry, 1);
       }
     }],
   };
   discovery.onPeer(peer => {
     peers.set(peer.id, peer);
-    peer.serve(server);
-    peer.pushClient = peer.client(server);
+    peer.serve(serverDefinition);
+    peer.pushClient = peer.client(serverDefinition);
   });
   const sendEntry = entry => {
     for (peer of peers) {
@@ -31,64 +27,80 @@ function Gossip(discovery, integrityKeys) {
   return {onEntry: entryWatcher.watch, sendEntry};
 }
 
-function update(entry, crdtTable, entries, apply) {
-  if (entry.op == 'create') {
-    return apply(crdtTable.get(entry.clock));
-  } else if (entry.op == 'update') {
-    return update(entries.get(entry.target), crdtTable, entries, x => entry.value);
-  } else if (entry.op == 'addField') {
-    return update(entries.get(entry.target), crdtTable, entries, x => {
-      x[entry.name] = apply(x[entry.name]);
-      return x;
-    });
+Id = PackedNumber({
+  clock: PackedNumber.Field(1, 40),
+  site: PackedNumber.Field(2, 17),
+  local: PackedNumber.Field(3, 20),
+})
+Create = Struct({
+  table: String(),
+  value: JsonBlob(),
+  after: Id(),
+})
+Delete = Struct({
+  target: Id(),
+})
+Update = Struct({
+  target: Id(),
+  value: JsonBlob(),
+})
+Entry = Struct({
+  id: Id(1),
+  op: OneOf(2, [Create, Delete, Update])
+})
+
+function update(obj, path, value) {
+  var cursor = obj;
+  for (const path of field.path.slice(0, -1)) {
+    if (!(cursor[path] instanceof Object)) {
+      cursor[path] = {};
+    }
+    cursor = cursor[path];
   }
+  cursor[field.path[field.path.length - 1]] = value;
+  return obj;
 }
 
-function Crdt(gossip, readKey) {
-  const tables = new DefaultMap(CrdtTable);
+function CrdtTable(idb, table) {
+  return {get, getVersions, getAll, watch, update, delete};
+}
+
+function Crdt(gossip, idb) {
+  const tables = new DefaultMap(table => CrdtTable(idb, table));
   const entries = new Map();
   gossip.onEntry(entry => {
     const op = entry.op;
-    if (op == 'create') {  // create(table, value, after)
+    if (op.$type == Create) {
       const table = tables.get(entry.table);
-      table.insert(entry.clock, entry.value, translateToNowRef(entry.after));
-    } else if (op == update) {  // update(target, newValue)
-      // [update, addField, update-winner]
-      if (clockSmaller(entries.getLastUpdate(entry.target).clock, entry.clock)) {
-        // walk up and down the path
-        const rootKey = not-sure-todo;
-        const obj = table.get(rootKey);
-        table.update(rootKey, obj);
+      table.insert(entry.id, op.value, op.after);
+    } else if (op.$type == Update) {
+      const table = tables.getByRow(op.target);
+      const obj = table.get(op.target);
+      const versions = table.getVersions(op.target);
+      for(const [field, value] of fields(op.value)) {
+        if (versions[field.name] < entry.id) {
+          versions[field.name] = entry.id;
+          obj = update(obj, field.path, value);
+        }
       }
-    } else if (op == addField) {  // addField(target, name, value)
-      // [addField, update, addField-winner]
-      if (isLastAddField(name)) {
-        table.update();
-      }
-    } else if (op == delete) {  // delete(target)
+      table.update(op.target, obj);
+      table.setVersions(op.target, versions);
+    } else if (op.$type == Delete) {
+      const table = tables.getByRow(op.target);
+      table.delete(op.target);
     }
   });
   return {table: tables.get};
 }
 
-function CrdtTable() {
-  return {get, getAll, watch, update, insert};
+function Database(crdt, idb, indices) {
+  return {set, merge, index, map, filter, group, sort, insert, update, delete};
 }
 
-function Database(crdt, idb, indices) {
-  return {set, merge, index, map, filter, group, sort};
-}
+Database(Crdt(Gossip(Discovery(server, name)), idb), indexIdb, {index});
 
 /*
 replication data model
-  tree of edits, siblings have an order: [global-clock, site-id, local-clock]
-    sum(sort(edits)) is actual data
-    see causal lib
-  operations
-    create(table, value, after)
-    update(op, new-value)
-    addField(op, field, value)
-    delete(op)
   how are arrays represented
     explicit
       crdt maintains order
@@ -98,22 +110,15 @@ replication data model
     independent (discarded)
       $ordering sortable string
       $ordering comparable object (hash lookup into tree walk)
+
+encryption - do later!
+  peerKey: talk to peers - Discovery
+  integrityKeys: sign content - Gossip
+  readKey: decrypt contents - Crdt
+
+out of scope
   moves: don't care
-  what is an id?
-    bigint - 40 / 17 / 20
-    encoded as a 13 character id
-  transaction - can't happen since history is not linear
-    logically it's a simultaneous set of read & writes
-    update([read-ops], [write-ops], [values])
-
-actual data
-  data = Map<Key, Val>
-  order maintenance problem
-    insertAfter(x, entry)
-    find(x) - returns an iterator
-    isBefore(x, y)
-
-after (order) vs set (unordered)
-  
-
-oneof
+  transaction:
+    can't happen since history is not linear
+    can master elect, but master cant guarantee know everything (what if ignored?)
+    would need update([read-ops], [write-ops], [values])
