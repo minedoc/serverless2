f672caef7c6216adee9fbc1870b2772214277c71
david <david>
2020-01-13 23:56:37 +1100

    add sort-maintaing method

--- summary.js
+++ summary.js
@@ -27,98 +27,207 @@ function Gossip(discovery) {
   return {onEntry: entryWatcher.watch, sendEntry};
 }
 
-Id = PackedNumber({
-  clock: PackedNumber.Field(1, 40),
-  site: PackedNumber.Field(2, 17),
-  local: PackedNumber.Field(3, 20),
+Id = Binary()
+CreateTable = Struct({
+  // no name!
+  ordered: Bool(),
 })
-Create = Struct({
-  table: String(),
-  value: JsonBlob(),
-  after: Id(),
+Insert = Struct({
+  value: JsonObject(),
+  target: Id(),  // ordered ? Insert/CreateTable : CreateTable
+})
+Update = Struct({
+  target: Id(),
+  value: JsonObject(),
 })
 Delete = Struct({
   target: Id(),
 })
-Update = Struct({
-  target: Id(),
-  value: JsonBlob(),
-})
 Entry = Struct({
-  id: Id(1),
-  op: OneOf(2, [Create, Delete, Update])
+  clock: PackedInt(1, {
+    clock: PackedInt.Field(1, 40),
+    site: PackedInt.Field(2, 8),
+    local: PackedInt.Field(3, 16),
+  }),
+  op: OneOf(2, [CreateTable, Insert, Update, Delete])
 })
 
-function update(obj, path, value) {
-  var cursor = obj;
-  for (const path of field.path.slice(0, -1)) {
-    if (!(cursor[path] instanceof Object)) {
-      cursor[path] = {};
+db = Database({
+  crdt: Crdt(...),
+  aliases: {
+    foo: Entry(clock)
+  },
+})
+db.dict('foo').map(extend).filter(conditions).group(x => x.name) : Dict
+  key
+db.dict('foo').sort(byDate) : SortedDict
+  iterate, key
+db.list('foo').map(extend).filter(conditions) : SortedDict
+  iterate, key
+
+function NestedSortedDict(items) {
+  const rows = new Map();
+
+  function load(items) {
+    const after = new MapSet();
+    for (const row of items) {
+      const ref = {row, after: SortedDict()}
+      rows.set(row.$id, ref);
+      after.add(row.$after, ref);
     }
-    cursor = cursor[path];
+    for (const [id, afterItems] of after) {
+      rows.get(id).after.insertMany(afterItems);
+    }
   }
-  cursor[field.path[field.path.length - 1]] = value;
-  return obj;
+  function get(key) {
+    return rows.get(key).row;
+  }
+  function insert(key, row) {
+    const ref = {row, after: SortedDict()};
+    rows.set(key, ref);
+    rows.get(row.$after).after.insert(row);
+  }
+  function update(key, row) {
+    rows.get(key).row = row;
+  }
+  function asDict() {
+  }
+  function asList() {
+    // iterate
+  }
+  return {get, insert, update, asDict, asList, load}
 }
 
-function CrdtTable(idb, table) {
-  return {get, getVersions, getAll, watch, update, delete};
+async function CrdtTable(idb, tableId) {
+  const watchers = new Set();
+  const notify = (...params) => watchers.forEach(w => w(...params));
+  const index = NestedSortedDict().load(await db.getAll(tableId));
+
+  async function get(key) {
+    return index.get(key);
+  }
+  async function getDict() {
+    return index.asDict();
+  }
+  async function getList() {
+    return index.asList();
+  }
+  async function insert(key, row) {
+    notify(INSERT, key, row);
+    index.insert(key, row);
+    return db.put(tableId, row, key);
+  }
+  async function update(key, row) {
+    notify(UPDATE, key, row);
+    index.update(key, row);
+    return db.put(tableId, row, key);
+  }
+  async function delete(key) {
+    notify(DELETE, key);
+    return db.put(tableId, null, key);
+  }
+  async function watch(watcher) {
+    watchers.add(watcher);
+  }
+  return {get, iterate, insert, update, delete, watch};
+}
+
+function WorkQueue(idb, process) {
+  const pending = new MapSet();
+  function enqueue(entry) {
+    if (!entry.op.target || tableFromId(entry.op.target)) {
+      recursiveEnqueue(entry);
+    } else {
+      pending.add(entry.op.target, entry);
+    }
+  }
+  function recursiveEnqueue(entry) {
+    process(entry);
+    const id = hash(entry);
+    if (pending.has(id)) {
+      pending.remove(id).forEach(e => recursiveEnqueue(e));
+    }
+  }
+  return {enqueue};
 }
 
 function Crdt(gossip, idb) {
-  const tables = new DefaultMap(table => CrdtTable(idb, table));
-  const entries = new Map();
-  gossip.onEntry(entry => {
+  const tableFromId = id => new CrdtTable(TODO);
+  const workQueue = WorkQueue(idb, entry => {
     const op = entry.op;
-    if (op.$type == Create) {
-      const table = tables.get(entry.table);
-      table.insert(entry.id, op.value, op.after);
+    const id = hash(entry);
+    const table = tableFromId(op.target);
+    if (op.$type == Insert) {
+      table.insert(id, Object.assign(op.value, {$clock: entry.clock, $id: id, $after: op.target}));
     } else if (op.$type == Update) {
-      const table = tables.getByRow(op.target);
-      const obj = table.get(op.target);
-      const versions = table.getVersions(op.target);
-      for(const [field, value] of fields(op.value)) {
-        if (versions[field.name] < entry.id) {
-          versions[field.name] = entry.id;
-          obj = update(obj, field.path, value);
-        }
+      if (row == null) {
+        return;
+      } else if (row.$clock < entry.clock) {
+        table.update(op.target, Object.assign(row, op.value, {$clock: entry.clock});
+      } else {
+        table.update(op.target, merge(table.getUpdates(op.target), entry));
       }
-      table.update(op.target, obj);
-      table.setVersions(op.target, versions);
     } else if (op.$type == Delete) {
-      const table = tables.getByRow(op.target);
       table.delete(op.target);
     }
   });
-  return {table: tables.get};
+  gossip.onEntry(entry => workQueue.enqueue(entry));
+  return tableFromId;
 }
 
-function Database(crdt, idb, indices) {
-  return {set, merge, index, map, filter, group, sort, insert, update, delete};
+function Database(crdt) {
+  return {
+    map, filter, group, sort,
+    insert, update, delete
+  };
 }
 
-Database(Crdt(Gossip(Discovery(server, name)), idb), indexIdb, {index});
+Database(Crdt(Gossip(Discovery(server, name)), idb));
 
 /*
-replication data model
+out of scope
+  moves
+    for arrays we sort of care -> move = (copy + delete) means edits are lost
+    maybe a moved-tombstone that proxies edits to new place
+  transaction
+    can't happen since history is not linear
+    can master elect, but master cant guarantee know everything (what if ignored?)
+    would need update([read-ops], [write-ops], [values])
+  indexes
+    we expect everything to fit in memory
+    filter is incremental
+
+how to persist order in database
+  map<Key -> (A, B)>
+  map<A -> C>
+  compare: C < C' or (C == C' and B < B')
+  insert:
+    prev -> (A, B)
+    update (A, b = b+1 where b > B)
+    if too many B in A
+      split A
+  insert requires updating(B/2 + B/2) items (update B, update A)
+
+how to represent local edits
+  these changes not written to the database - edit-before-save, local options
+  join against local-db
+
+encryption - do later!
+  x -> peerKey: talk to peers - Discovery
+  w -> integrityKeys: sign content - Gossip
+  r -> readKey: decrypt contents - Crdt
+
+decisions
+  what happens if duplicate id seen
+    content-signature id
+      not trivially collidable - need some effort
+    user-provided id (discarded)
+      con: trivially collidable
   how are arrays represented
     explicit
       crdt maintains order
       getAll returns [...] with ordering applied
       watch insert events have a position
-      trivial to prepend list, but append is difficult?
     independent (discarded)
-      $ordering sortable string
-      $ordering comparable object (hash lookup into tree walk)
-
-encryption - do later!
-  peerKey: talk to peers - Discovery
-  integrityKeys: sign content - Gossip
-  readKey: decrypt contents - Crdt
-
-out of scope
-  moves: don't care
-  transaction:
-    can't happen since history is not linear
-    can master elect, but master cant guarantee know everything (what if ignored?)
-    would need update([read-ops], [write-ops], [values])
+      $ordering string
+      $ordering object (hash lookup into tree walk)
