a78ff3bbe18ddf966de7454e8988caef05a38cd1
david <david>
2021-02-17 02:05:06 +1100

    show status

--- init.js
+++ init.js
@@ -4,6 +4,9 @@ async function init() {
   window.connection = newConnectionString();
   console.log(connection);
   window.db = await Database('foo', '62pZn1kyWFZkSaEvcTd-646o3N0QsE-cffRc4CaRfG');
+  setInterval(() => {
+    document.getElementById('out').innerText = 'foo = ' + JSON.stringify(Array.from(db.table('foo')));
+  }, 1000);
 }
 
 export {init};
